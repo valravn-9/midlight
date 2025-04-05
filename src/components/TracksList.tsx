7dcddc0a07d12aa2e6e5aca6a6184bd49f40d413
Daniel Brandl <daniel.brandl@haufe-akademie.de>
2025-04-05 22:52:02 +0200

    feat: add empty state component to TracksList for better user experience

--- src/components/TracksList.tsx
+++ src/components/TracksList.tsx
@@ -1,7 +1,9 @@
-import { FlatList, FlatListProps, View } from 'react-native';
+import { FlatList, FlatListProps, Text, View } from 'react-native';
 import { TrackListItem } from './TrackListItem';
 import { utilsStyles } from '@/styles';
 import TrackPlayer, { Track } from 'react-native-track-player';
+import FastImage from 'react-native-fast-image';
+import { unknownTrackImageUri } from '@/constants/images';
 
 export type TracksListProps = Partial<FlatListProps<Track>> & {
   tracks: Track[];
@@ -23,6 +25,15 @@ export const TracksList = ({ tracks, ...flatlistProps }: TracksListProps) => {
       contentContainerStyle={{ paddingTop: 10, paddingBottom: 128 }}
       ListFooterComponent={ItemDivider}
       ItemSeparatorComponent={ItemDivider}
+      ListEmptyComponent={
+        <View>
+          <Text style={utilsStyles.emptyContentText}>No songs found</Text>
+          <FastImage
+            source={{ uri: unknownTrackImageUri, priority: FastImage.priority.normal }}
+            style={utilsStyles.emptyContentImage}
+          />
+        </View>
+      }
       renderItem={({ item: track }) => (
         <TrackListItem track={track} onTrackSelect={handleTrackSelect} />
       )}
