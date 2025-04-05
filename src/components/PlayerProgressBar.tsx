3f3c4f5f9f85942274d5fe48ed5b77a98b8f8e1c
Daniel Brandl <daniel.brandl@haufe-akademie.de>
2025-04-06 01:42:59 +0200

    fix: optimize progress calculation in PlayerProgressBar component

--- src/components/PlayerProgressBar.tsx
+++ src/components/PlayerProgressBar.tsx
@@ -3,23 +3,21 @@ import { formatSecondsToMinutes } from '@/helpers/miscellaneous';
 import { defaultStyles, utilsStyles } from '@/styles';
 import { View, ViewProps, Text, StyleSheet } from 'react-native';
 import { Slider } from 'react-native-awesome-slider';
-import { useSharedValue } from 'react-native-reanimated';
+import { useSharedValue, useDerivedValue } from 'react-native-reanimated';
 import TrackPlayer, { useProgress } from 'react-native-track-player';
 
 export const PlayerProgressBar = ({ style }: ViewProps) => {
   const { duration, position } = useProgress(250);
 
   const isSliding = useSharedValue(false);
-  const progress = useSharedValue(0);
+  const progress = useDerivedValue(() => {
+    return isSliding.value ? 0 : duration > 0 ? position / duration : 0;
+  });
   const min = useSharedValue(0);
   const max = useSharedValue(1);
 
   const trackElapsedTime = formatSecondsToMinutes(position);
   const trackRemainingTime = formatSecondsToMinutes(duration - position);
-
-  if (!isSliding.value) {
-    progress.value = duration > 0 ? position / duration : 0;
-  }
 
   return (
     <View style={[style]}>
