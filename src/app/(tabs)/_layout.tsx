5380c8b143a096eb1fcb8bff42e77bd0c62544d8
Daniel Brandl <daniel.brandl@haufe-akademie.de>
2025-04-05 13:41:41 +0200

    feat: enhance tab navigation with custom screen options for active tint color and label styles

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,8 +1,18 @@
+import { colors, fontSize } from '@/constants/tokens';
 import { Tabs } from 'expo-router';
 
 const TabsNavigation = () => {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: colors.primary,
+        tabBarLabelStyle: {
+          fontSize: fontSize.xs,
+          fontWeight: '500',
+        },
+        headerShown: false,
+      }}
+    >
       <Tabs.Screen name="favorites" />
       <Tabs.Screen name="playlists" />
       <Tabs.Screen name="(songs)" />
