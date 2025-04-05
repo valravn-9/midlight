0a29e362542883b66a146a0056a84c63faec7171
Daniel Brandl <daniel.brandl@haufe-akademie.de>
2025-04-05 21:38:19 +0200

    feat: add track selection functionality to TrackListItem and TracksList components

--- src/components/TracksList.tsx
+++ src/components/TracksList.tsx
@@ -12,13 +12,19 @@ const ItemDivider = () => (
 );
 
 export const TracksList = ({ tracks, ...flatlistProps }: TracksListProps) => {
+  const handleTrackSelect = (track: Track) => {
+    console.log(track);
+  };
+
   return (
     <FlatList
       data={tracks}
       contentContainerStyle={{ paddingTop: 10, paddingBottom: 128 }}
       ListFooterComponent={ItemDivider}
       ItemSeparatorComponent={ItemDivider}
-      renderItem={({ item: track }) => <TrackListItem track={track} />}
+      renderItem={({ item: track }) => (
+        <TrackListItem track={track} onTrackSelect={handleTrackSelect} />
+      )}
       {...flatlistProps}
     />
   );
