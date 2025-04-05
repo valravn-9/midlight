bcd2e24d6344b3001c870992f233074470ff8117
Daniel Brandl <daniel.brandl@haufe-akademie.de>
2025-04-05 14:37:10 +0200

    feat: enhance TracksList and TrackListItem components with artist display and improved styling

--- src/components/TracksList.tsx
+++ src/components/TracksList.tsx
@@ -1,6 +1,19 @@
 import { FlatList } from 'react-native';
 import library from '@/assets/data/library.json';
+import { TrackListItem } from './TrackListItem';
 
 export const TracksList = () => {
-  return <FlatList data={library} renderItem={() => null} />;
+  return (
+    <FlatList
+      data={library}
+      renderItem={({ item: track }) => (
+        <TrackListItem
+          track={{
+            ...track,
+            image: track.artwork,
+          }}
+        />
+      )}
+    />
+  );
 };
