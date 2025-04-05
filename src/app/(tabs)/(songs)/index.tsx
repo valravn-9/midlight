5af60e8d9b2c38b440b46c401e9fc0417fdb10d2
Daniel Brandl <daniel.brandl@haufe-akademie.de>
2025-04-05 15:13:56 +0200

    feat: implement track filtering in Songs screen and update TracksList to accept filtered tracks

--- src/app/(tabs)/(songs)/index.tsx
+++ src/app/(tabs)/(songs)/index.tsx
@@ -3,6 +3,9 @@ import { screenPadding } from '@/constants/tokens';
 import { useNavigationSearch } from '@/hooks/useNavigationSearch';
 import { defaultStyles } from '@/styles';
 import { ScrollView, Text, View } from 'react-native';
+import library from '@/assets/data/library.json';
+import { useMemo } from 'react';
+import { trackTitleFilter } from '@/helpers/filter';
 
 const SongsScreen = () => {
   const search = useNavigationSearch({
@@ -11,13 +14,19 @@ const SongsScreen = () => {
     },
   });
 
+  const filteredTracks = useMemo(() => {
+    if (!search) return library;
+
+    return library.filter(trackTitleFilter(search));
+  }, [search]);
+
   return (
     <View style={defaultStyles.container}>
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={{ paddingHorizontal: screenPadding.horizontal }}
       >
-        <TracksList scrollEnabled={false} />
+        <TracksList tracks={filteredTracks} scrollEnabled={false} />
       </ScrollView>
     </View>
   );
--- src/components/TracksList.tsx
+++ src/components/TracksList.tsx
@@ -1,19 +1,21 @@
 import { FlatList, FlatListProps, View } from 'react-native';
-import library from '@/assets/data/library.json';
 import { TrackListItem } from './TrackListItem';
 import { utilsStyles } from '@/styles';
 
-export type TracksListProps = Partial<FlatListProps<unknown>>;
+export type TracksListProps = Partial<FlatListProps<unknown>> & {
+  tracks: any[];
+};
 
 const ItemDivider = () => (
   <View style={{ ...utilsStyles.itemSeparator, marginVertical: 9, marginLeft: 60 }} />
 );
 
-export const TracksList = ({ ...flatlistProps }: TracksListProps) => {
+export const TracksList = ({ tracks, ...flatlistProps }: TracksListProps) => {
   return (
     <FlatList
-      data={library}
+      data={tracks}
       contentContainerStyle={{ paddingTop: 10, paddingBottom: 128 }}
+      ListFooterComponent={ItemDivider}
       ItemSeparatorComponent={ItemDivider}
       renderItem={({ item: track }) => (
         <TrackListItem
