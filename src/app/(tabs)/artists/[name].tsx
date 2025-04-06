2973ffb0dd9ab7d08fb6f1c102ccb7c0825f0d98
Daniel Brandl <daniel.brandl@haufe-akademie.de>
2025-04-06 12:59:49 +0200

    feat: add ArtistDetailScreen and ArtistTracksList components for artist-specific track display

---
+++ src/app/(tabs)/artists/[name].tsx
@@ -0,0 +1,32 @@
+import { ArtistTracksList } from '@/components/ArtistTracksList';
+import { screenPadding } from '@/constants/tokens';
+import { useArtists } from '@/store/library';
+import { defaultStyles } from '@/styles';
+import { Redirect, useLocalSearchParams } from 'expo-router';
+import { ScrollView, View } from 'react-native';
+
+const ArtistDetailScreen = () => {
+  const { name: artistName } = useLocalSearchParams<{ name: string }>();
+
+  const artists = useArtists();
+
+  const artist = artists.find((artist) => artist.name === artistName);
+
+  if (!artist) {
+    console.warn(`Artist with name ${artistName} not found`);
+    return <Redirect href="/(tabs)/artists" />;
+  }
+
+  return (
+    <View style={defaultStyles.container}>
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={{ paddingHorizontal: screenPadding.horizontal }}
+      >
+        <ArtistTracksList artist={artist} />
+      </ScrollView>
+    </View>
+  );
+};
+
+export default ArtistDetailScreen;
--- src/app/(tabs)/artists/_layout.tsx
+++ src/app/(tabs)/artists/_layout.tsx
@@ -1,4 +1,5 @@
 import { StackScreenWithSearchBar } from '@/constants/layout';
+import { colors } from '@/constants/tokens';
 import { defaultStyles } from '@/styles';
 import { Stack } from 'expo-router';
 import { View } from 'react-native';
@@ -10,6 +11,17 @@ const ArtistsScreenLayout = () => {
         <Stack.Screen
           name="index"
           options={{ ...StackScreenWithSearchBar, headerTitle: 'Artists' }}
+        />
+        <Stack.Screen
+          name="[name]"
+          options={{
+            headerTitle: '',
+            headerBackVisible: true,
+            headerStyle: {
+              backgroundColor: colors.background,
+            },
+            headerTintColor: colors.primary,
+          }}
         />
       </Stack>
     </View>
