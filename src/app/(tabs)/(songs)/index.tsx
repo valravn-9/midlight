4303a8d4693c1970c107732875c8bc21433ad1cf
Daniel Brandl <daniel.brandl@haufe-akademie.de>
2025-04-06 02:38:06 +0200

    feat: refactor SongsScreen to use Zustand for track management and update filtering logic

--- src/app/(tabs)/(songs)/index.tsx
+++ src/app/(tabs)/(songs)/index.tsx
@@ -3,9 +3,9 @@ import { screenPadding } from '@/constants/tokens';
 import { useNavigationSearch } from '@/hooks/useNavigationSearch';
 import { defaultStyles } from '@/styles';
 import { ScrollView, View } from 'react-native';
-import library from '@/assets/data/library.json';
 import { useMemo } from 'react';
 import { trackTitleFilter } from '@/helpers/filter';
+import { useTracks } from '@/store/library';
 
 const SongsScreen = () => {
   const search = useNavigationSearch({
@@ -14,11 +14,13 @@ const SongsScreen = () => {
     },
   });
 
-  const filteredTracks = useMemo(() => {
-    if (!search) return library;
+  const tracks = useTracks();
 
-    return library.filter(trackTitleFilter(search));
-  }, [search]);
+  const filteredTracks = useMemo(() => {
+    if (!search) return tracks;
+
+    return tracks.filter(trackTitleFilter(search));
+  }, [search, tracks]);
 
   return (
     <View style={defaultStyles.container}>
