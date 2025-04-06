b7ed9e53781c40d36798a03363b8163df93f2a92
Daniel Brandl <daniel.brandl@haufe-akademie.de>
2025-04-06 02:02:08 +0200

    feat: enhance FavoritesScreen with search functionality and display of favorite tracks

--- src/app/(tabs)/favorites/index.tsx
+++ src/app/(tabs)/favorites/index.tsx
@@ -1,10 +1,30 @@
+import { TracksList } from '@/components/TracksList';
 import { defaultStyles } from '@/styles';
-import { Text, View } from 'react-native';
+import { ScrollView, View } from 'react-native';
+import library from '@/assets/data/library.json';
+import { screenPadding } from '@/constants/tokens';
+import { useMemo } from 'react';
+import { useNavigationSearch } from '@/hooks/useNavigationSearch';
 
 const FavoritesScreen = () => {
+  const search = useNavigationSearch({
+    searchBarOptions: {
+      placeholder: 'Find in songs',
+    },
+  });
+
+  const favoritesTracks = useMemo(() => {
+    return library.filter((track) => track.rating === 1);
+  }, []);
+
   return (
     <View style={defaultStyles.container}>
-      <Text style={defaultStyles.text}>Favorites screen</Text>
+      <ScrollView
+        style={{ paddingHorizontal: screenPadding.horizontal }}
+        contentInsetAdjustmentBehavior="automatic"
+      >
+        <TracksList scrollEnabled={false} tracks={favoritesTracks} />
+      </ScrollView>
     </View>
   );
 };
